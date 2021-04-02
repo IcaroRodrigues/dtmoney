731c970c04ed2373a7c0aa2de0ffd73c2df85547
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2021-04-02 17:32:04 -0300

    Adiciona funcionalidades e estilos para botões de tipo de transação

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
 export const Container = styled.form`
   h2 {
@@ -57,34 +57,44 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
 
-    background: transparent;
-  
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
+}
 
-    &:hover {
-      transition: 0.2s;
-      border-color: ${darken(0.1, '#d7d7d7')};
-    }
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  background: ${({isActive, activeColor}) => isActive ? transparentize(0.9, colors[activeColor]) : 'trasparent'};
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+  &:hover {
+    transition: 0.2s;
+    border-color: ${darken(0.1, '#d7d7d7')};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
